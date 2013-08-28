04b479346faa6649c621876d66ee8a5dff399edd
Brian Han <brianhan87usa@gmail.com>
2013-08-28 00:13:19 -0400

    Add length of tweets stat

--- twitter_viz/nokogiri_test.rb
+++ twitter_viz/nokogiri_test.rb
@@ -5,10 +5,16 @@ require 'open-uri'
 puts "Enter full twitter handle:"
 twitter_handle = gets.chomp
 
-File.open('books.html', 'w') do |f|
+File.open('twitter_viz.html', 'w') do |f|
 	page = Nokogiri::HTML(open("https://twitter.com/" + twitter_handle))
-	tweets         = page.css('.tweet-text')
+	
 	twitter_info = page.css('.profile-field')
+	tweets       = page.css('.tweet p')
+	tweet_length  = 0
+	tweets.each do |tweet| 
+		length = tweet.to_s.length
+		tweet_length += length
+	end
 
 	f.write "<html>\n"
 	f.write "\t<head>\n"
@@ -17,11 +23,16 @@ File.open('books.html', 'w') do |f|
 	f.write "\t<link rel='stylesheet' type='text/css' href='css/bootstrap.css'>\n"
 	f.write "</head>\n"
 	f.write "<body>\n"
-	
 	f.write "\t" + '<div class="hero-unit">' + twitter_info.to_s + '</div>' + "\n"
-	f.write "\t" + '<div class="container">' 
-	tweets.each   { |tweet| f.write("\t<div class='well well-large'><p>" + tweet.to_s + "</p></div>\n") }
-	
+	# f.write "\t" + '<div class="container">' 
+	f.write "\t" + '<div class="hero-unit">' + "<h2>Length of Tweets on Page: #{tweet_length}</h2>" + '</div>' + "\n"
+	# f.write "\t" + '<div class="container">' 
+	# number_of_tweets = 0
+	tweets.each do |tweet| 
+		f.write("\t<div class='well well-large'>" + tweet.to_s + "</div>\n")
+		# number_of_tweets += 1
+	end 
+	# f.write("\t<h1>Number of Tweets: #{number_of_tweets}</h1>\n")
 	f.write "\t</div>\n"
 	f.write "</body>\n"
 	f.write "</html>\n" 
